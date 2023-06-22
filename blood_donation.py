6885338e9037e601820d883b792b80cd2955f03d
Amuthini-P <133939136+Amuthini-P@users.noreply.github.com>
2023-06-22 11:20:40 +0530

    Add files via upload

---
+++ blood_donation.py
@@ -0,0 +1,7 @@
+name=input("enter the name:")
+age=int(input("enter the age:"))
+weight=int(input("enter the weight:"))
+if age>18 and weight>40:
+    print("eligible for blood donation")
+else:
+    print("not eligible for blood donation")
